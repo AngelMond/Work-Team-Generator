60f671ec31f56e3e9c57afe4cf70d497add4fe20
AngelMond <abraham_will@hotmail.com>
2022-05-28 21:52:44 -0500

    Creating code to generate info for the team MANAGER and list to choose add engineer or intern created too

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,39 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
+console.log('Welcome!')
 inquirer
   .prompt([
     /* Pass your questions in here */
+
+    {
+        type: 'input',
+        name: 'ManagersName',
+        message: 'Please enter the Managers name',
+    },
+    {
+        type: 'input',
+        name: 'EmployeeId',
+        message: 'Enter the employee ID',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the Manager email address',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'For the last, please enter the office number',
+        
+    },
+    {
+        type:'rawlist',
+        name:'ListOptions',
+        message: 'What would you like to do? You can add more team members or finish your team',
+        choices: ['Add an Engineer' , 'Add an Intern', 'Finish',],
+    }
+    
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
@@ -16,3 +46,17 @@ inquirer
       // Something else went wrong
     }
   });
+
+  function engineer(){
+    inquirer
+    .prompt([
+      /* Pass your questions in here */
+  
+      {
+          type: 'input',
+          name: 'ManagersName',
+          message: 'Please enter the Managers name',
+      }
+    ])
+
+  }
